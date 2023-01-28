9603327afcf115fdb14caee86713fbf5252ec7fe
AnastasiyaP <pelevinaav@mail.ru>
2023-01-28 16:58:02 +0300

    add Metod

--- Program.cs
+++ Program.cs
@@ -2,3 +2,16 @@
 
 string[] array1 = new string[8] {"98754", "11", "super", "kross", "peas", "F2q", "17fa55", "5"};
 string[] array2 = new string[array1.Length];
+
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
